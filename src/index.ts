5a74b5f4dab9f99b6438f3e960963a40a81655f8
cs-fedy <fedi.abd001@gmail.com>
2024-12-06 07:12:48 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ dotenv.config()
 const app = express()
 
 app.set("trust proxy", true)
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use(express.json())
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")))
